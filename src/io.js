519c67edae09508fb8a37fd4537169d1d05230e3
Pablo Moreno <pablo@kasfactory.net>
2019-04-02 17:27:50 +0200

    socket io changes

--- src/io.js
+++ src/io.js
@@ -14,29 +14,37 @@ io.sockets.on('connection', function (socket) {
   })
 
   socket.on('unsubscribe', function(room) {
-    console.log('Leaving room: ', room)
+    console.log('Leaving room: ', room._id)
     socket.leave(room)
   })
   
   socket.on('ack-user-connected', async function (user) {
-    console.log('[ACK] - User-connected', user)
+    console.log('[ACK] - User-connected', user.id, user.username)
     const rooms = await Room.findByUser(user)
     console.log('Rooms', rooms)
     socket.emit('user-rooms', rooms)
   })
 
   socket.on('post-message', async function ({ text, user, room }) {
-    console.log('post-message', { text, user, room })
-    const msg = new Message({ text, user: user.id, room: room._id })
+    console.log('post-message', user.username, text, room)
+    const msg = new Message({ text, user: user.id, room: room })
     const result = await msg.save()
-    io.sockets.in(room._id).emit('new-message', { 
+    io.sockets.in(room).emit('new-message', { 
       text, 
       user: {
         id: user.id,
         username: user.username,
       },
-      room: room._id
+      room: room
     })
+  })
+
+  socket.on('writing-message', async function({ user, room }) {
+    io.sockets.in(room).emit('user-is-writing', { user, room })
+  })
+
+  socket.on('stop-writing', async function({ user, room }) {
+    io.sockets.in(room).emit('user-stopped-writing', { user, room })
   })
 })
 
