32d815e56c50838b4df90a07386e67636fb18a52
Pablo Moreno <pablomoreno.inf@gmail.com>
2019-04-07 14:58:40 +0200

    Sign up test added

--- src/tests/api/index.test.js
+++ src/tests/api/index.test.js
@@ -3,7 +3,18 @@ import request from 'supertest'
 
 describe('API calls', () => {
   test('Sign up', async () => {
-    const response = await request(server).get('/auth/sign-up')
-    expect(response.status).toBe(404)
+    const response = await request(server)
+      .post('/auth/sign-up')
+      .send({ 
+        username: 'chewie',
+        password: 'gruaaargh',
+        password2: 'gruaaargh',
+        email: 'chewie@mail.com'
+      })
+    const user = response.body
+    
+    expect(response.status).toBe(200)
+    expect(user.username).toBe('chewie')
+    expect(user.email).toBe('chewie@mail.com')
   })
 })
