2bf35c8e24f763a39471850423c4f0a04075e152
Pablo Moreno <pablo@kasfactory.net>
2019-04-08 16:47:44 +0200

    Moved urls to server.js and started documenting endpoints

--- src/server.js
+++ src/server.js
@@ -5,10 +5,13 @@ import bodyParser from 'body-parser'
 import helmet from 'helmet'
 import compression from 'compression'
 import multer from 'multer'
-import routes from './routes'
 import { DEBUG, SECRET_KEY, UPLOADS_PATH } from './config'
 import { authenticationRequired, checkPermissions, logger } from './middleware'
 import { errorWrapper } from './utils'
+import { login, createUser, getMe, updateUser, uploadAvatar, createGroup, searchUsers } from './controllers/auth'
+import { getRooms, createRoom, updateRoom } from './controllers/rooms'
+import { canSeeRoom, canUpdateRoom, canDeleteRoom } from './permissions/rooms'
+import { createFriendshipRequest, acceptFriendshipRequest, refuseFriendshipRequest } from './controllers/chat'
 
 // Create server
 const server = express()
@@ -44,25 +47,91 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({ storage })
-
 server.use(session(sessionConf))
 
-routes.forEach(route => {
-  console.log('-', `[${route.method}]`, route.path)
+// Auth
 
-  const middleware = [
-    logger, 
-    route.protected ? authenticationRequired : undefined, 
-    checkPermissions(route.permissions),
-    route.upload ? upload.single(route.upload) : undefined,
-    ...(route.middleware ? route.middleware : [])
-  ].filter(m => m !== undefined)
+/**
+ * @api {post} /auth/login User login
+ * @apiName Authenticate user
+ * @apiGroup Authentication
+ * 
+ * @apiParam {String}     email                   User's email
+ * @apiParam {String}     password                User's password
+ * 
+ * @apiSuccess {String}     id                User's Id
+ * @apiSuccess {String}     username          User's username
+ * @apiSuccess {String}     email             User's email
+ * @apiSuccess {String}     token             User's token
+ * @apiSuccess {String}     firstName         User's first name
+ * @apiSuccess {String}     lastName          User's last name
+ */
+server.post('/auth/login', logger, checkPermissions([]), errorWrapper(login))
 
-  server[route.method.toLowerCase()](
-    route.path, 
-    ...middleware,
-    errorWrapper(route.controller),
-  )
-})
+/**
+ * @api {post} /auth/sign-up User registration
+ * @apiName Create new user
+ * @apiGroup Authentication
+ * 
+ * @apiSuccess {String}     username          User's username
+ * @apiSuccess {String}     password          User's password
+ * @apiSuccess {String}     password2         Repeat password
+ * @apiSuccess {String}     email             User's email
+ * @apiSuccess {String}     firstName         User's first name
+ * @apiSuccess {String}     lastName          User's last name
+ * 
+ * @apiSuccess {String}     id                User's Id
+ * @apiSuccess {String}     username          User's username
+ * @apiSuccess {String}     email             User's email
+ * @apiSuccess {String}     token             User's token
+ * @apiSuccess {String}     firstName         User's first name
+ * @apiSuccess {String}     lastName          User's last name
+ */
+server.post('/auth/sign-up', logger, checkPermissions([]), errorWrapper(createUser))
+server.get('/auth/me', logger, authenticationRequired, checkPermissions([]), errorWrapper(getMe))
+server.put('/auth/me', logger, authenticationRequired, checkPermissions([]), errorWrapper(updateUser))
+server.post('/auth/me/avatar', logger, authenticationRequired, checkPermissions([]), upload.single('avatar'), errorWrapper(uploadAvatar))
+server.post('/auth/groups/new', logger, authenticationRequired, checkPermissions([]), errorWrapper(createGroup))
+
+// Room
+
+/**
+ * @api {get} /rooms Get User Rooms
+ * @apiName Get User Rooms
+ * @apiGroup Rooms
+ * 
+ * @apiSuccess {String}     _id                     Room ID
+ * @apiSuccess {String}     name                    Room name
+ * @apiSuccess {Boolean}    group                   Is group
+ * @apiSuccess {String}     admin                   Admin's user id
+ * @apiSuccess {Object[]}   users                   Room users
+ * @apiSuccess {String}     users._id               User's Id
+ * @apiSuccess {String}     users.username          User's username
+ * @apiSuccess {String}     users.email             User's email
+ */
+server.get('/rooms', logger, authenticationRequired, checkPermissions([]), errorWrapper(getRooms))
+
+/**
+ * @api {post} /rooms Create new Room
+ * @apiName Create new Room
+ * @apiGroup Rooms
+ * 
+ * @apiParam {String}     name                    Room name
+ * @apiParam {Boolean}    group                   Is group
+ * @apiParam {String}     admin                   User's Id
+ * @apiParam {String[]}   users                   Room users ids
+ */
+server.post('/rooms', logger, authenticationRequired, checkPermissions([]), errorWrapper(createRoom))
+server.get('/rooms/:id', logger, authenticationRequired, checkPermissions([canSeeRoom]), errorWrapper(getRooms))
+server.put('/rooms/:id', logger, authenticationRequired, checkPermissions([canUpdateRoom]), errorWrapper(updateRoom))
+server.delete('/rooms/:id', logger, authenticationRequired, checkPermissions([canDeleteRoom]), errorWrapper(updateRoom))
+
+// Friendship requests
+server.post('/friendships/new', logger, authenticationRequired, checkPermissions([]), errorWrapper(createFriendshipRequest))
+server.post('/friendships/:id/accept', logger, authenticationRequired, checkPermissions([]), errorWrapper(acceptFriendshipRequest))
+server.post('/friendships/:id/refuse', logger, authenticationRequired, checkPermissions([]), errorWrapper(refuseFriendshipRequest))
+
+// Users
+server.get('/users', logger, authenticationRequired, checkPermissions([]), errorWrapper(searchUsers))
 
 export default server
